8974139ed1f8e11125c9b3cba336f42aee502edf
Josh Whitney <josh.whitney@transamerica.com>
2015-12-15 17:44:52 -0600

    Meat of get_level_1_data is finished.
    Tested and working for basic functionality.

--- R/level_1_data.R
+++ R/level_1_data.R
@@ -1,25 +1,34 @@
 #Add CV folds to the input data frame.
 get_cv_folds <- function(training_frame, n_folds){
   #Check to see if training_frame already has a CV column.  If not, add one.
-  if(is.null(attributes(training_frame)$CV_column)){
+  if(is.null(attributes(training_frame)$cv_column)){
     num_obs <- nrow(training_frame)
     # Cross-validation folds
     folds <- sample(rep(seq(n_folds), ceiling(num_obs/n_folds)))[1:num_obs]
     training_frame$fold_id <- folds
-    attributes(training_frame)$CV_column <- "fold_id"
+    attributes(training_frame)$cv_column <- "fold_id"
   }
   training_frame
 }
 
 
-get_level_1_data <- function(training_frame, model_wrappers, n_folds = 5){
-  #Add a folds column to keep track of CV folds.
+get_level_1_data <- function(training_frame,
+                             response,
+                             model_wrappers,
+                             n_folds = 5){
+  #Add a folds column to keep track of CV folds if it's not present already.
   training_frame <- get_cv_folds(training_frame, n_folds = n_folds)
-  setDT(training_frame)
+  dt <- as.data.table(training_frame)
 
   for(model_wrapper in model_wrappers){
+    #Each loop adds predictions for the fold under consideration, while all
+    #subsequent folds have NA placeholders.  Each time the loop repeats the NA's
+    #for the current fold are overwritten (by reference, thanks data.table!).
+    #You can watch this process occur with the debugger.
     for(fold in 1:n_folds){
-
+      dt[fold_id == fold,
+         (model_wrapper) := match.fun(model_wrapper)(training_frame = dt[fold_id != fold,],
+                                                     validation_frame = dt[fold_id == fold,])]
     }
   }
 }
